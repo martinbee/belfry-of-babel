cac46c0ee2153d141e2388094ae8be2702fe1261
martinbee <martin@poeticsystems.com>
2016-03-11 07:51:04 -0600

    make import statment work

--- client/startup.jsx
+++ client/startup.jsx
@@ -1,4 +1,4 @@
-import App from '/client/components/app/App';
+import App from './components/app/App';
 
 Meteor.startup(() => {
   ReactDOM.render(<App />, document.getElementById('render-target'));
